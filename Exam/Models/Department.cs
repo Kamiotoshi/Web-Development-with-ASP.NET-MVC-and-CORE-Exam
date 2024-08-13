e00f42380ffe9871cc6e9e04531f17bb06f09172
Kamiotoshi <133650059+Kamiotoshi@users.noreply.github.com>
2024-08-13 10:43:15 +0700

    Add files via upload

---
+++ Exam/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Exam.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Location { get; set; }
+        public int NumberOfPersonals { get; set; }
+
+        public ICollection<Employee>? Employees { get; set; }
+    }
+}
---
+++ Exam/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Exam.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Rank { get; set; }
+
+        public int DepartmentId { get; set; }
+        public Department? Department { get; set; }
+    }
+}
