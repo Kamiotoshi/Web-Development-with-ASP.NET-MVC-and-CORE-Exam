d98d45eb13dc6eacd24d3c44964acb75af1f6063
Kamiotoshi <133650059+Kamiotoshi@users.noreply.github.com>
2024-08-13 10:41:11 +0700

    Add files via upload

---
+++ Exam/Views/Reports/DepartmentStatistics.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Exam.Views.Reports
+{
+    public class DepartmentStatisticsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
